8ceba56a04754b17e7c2249d48739e90e1c6c6ec
Saurabh Tayde <47485215+SaurabhTayde@users.noreply.github.com>
2022-07-05 20:33:29 +0530

    Update ml_rest_client.py

--- ml_rest_client.py
+++ ml_rest_client.py
@@ -1,7 +1,8 @@
 import json
 import requests
 
-url = 'http://127.0.0.1:8005/model'
+#url = 'http://127.0.0.1:8005/model'
+url = 'http://34.125.94.179:8005/model'
 
 request_data = json.dumps({'age':40,'salary':20000})
 response = requests.post(url,request_data)
